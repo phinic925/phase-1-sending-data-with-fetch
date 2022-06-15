2e6ae87e0991b671d1ee4c8a683c7e8ddb066be8
phinic925 <phinicnyangate480@gmail.com>
2022-06-15 09:52:09 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+
+
+function submitData(name,email){
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+   "Content-Type":  "application/json",
+   Accept:  "application/json",
+        },
+        body: JSON.stringify({
+            name,email
+        })
+    })
+
+        .then(resp=> resp.json())
+        .then(Object=> {
+            document.body.innerHTML=Object["id"]
+         })
+          
+        .catch(error=> {
+            document.body.innerHTML=error.message
+
+
+        })
+           
+
+    
+    
+
+}
